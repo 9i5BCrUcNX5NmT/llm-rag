93699a95bebf8fa667da1b5ed6806913ff6cef92
9i5BCrUcNX5NmT <machinima40@yandex.ru>
2024-07-03 22:26:36 +0300

    Улучшение перевода

--- ml/lib.py
+++ ml/lib.py
@@ -1,12 +1,12 @@
 async def translate(first, second, text):
     from ollama import AsyncClient
 
+    translate_model_name = "thinkverse/towerinstruct"
+
     message = {
         "role": "user",
-        "content": f"Переведи следующий текст с {first} на {second}:\n{text}\n",
+        "content": f"Переведи следующий текст с {first} на {second}:\n{text}\n{second}:",
     }
-    translate = await AsyncClient().chat(
-        model="thinkverse/towerinstruct", messages=[message]
-    )
+    translate = await AsyncClient().chat(model=translate_model_name, messages=[message])
 
     return translate["message"]["content"]
